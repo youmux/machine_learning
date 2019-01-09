03e1caf03d86f2b83776e4c891350794d9806a85
youmux <1018472917@qq.com>
2019-01-09 13:03:09 +0800

    test_nn_lr.py 增加对比效果

--- codes/test_nn_lr.py
+++ codes/test_nn_lr.py
@@ -1,6 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from neural_networks_logistic_regression import NeuralNetworksLogisticRegression
+from logistic_regression import LogisticRegression
+
+# 绘制决策边界
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=np.squeeze(y), cmap=plt.cm.Spectral)    # show dataset points; y -> np.squeeze(y)
+
 
 # 加载数据
 def load_planar_dataset():
@@ -25,67 +45,40 @@ def load_planar_dataset():
     return X, Y
 
 
-# 绘制决策边界
-def plot_decision_boundary(model, X, y):
-    # Set min and max values and give it some padding
-    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
-    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
-    h = 0.01
-    # Generate a grid of points with distance h between them
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    # Predict the function value for the whole grid
-    Z = model(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    # Plot the contour and training examples
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-    plt.ylabel('x2')
-    plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=np.squeeze(y), cmap=plt.cm.Spectral)    # y -> np.squeeze(y)
-
-
 if __name__ == "__main__":
     # 加载数据
     X, Y = load_planar_dataset()
-
+    
     N = 20000   # 迭代次数
 
-    # 神经网络的逻辑回归
+    # 1.逻辑回归
+    lr = LogisticRegression()
+    lr.fit(X, Y)
+    lr.train(0.05, N)
+    predicted = lr.predict(X)   # 预测结果
+    print(f'逻辑回归的准确率:{np.mean(np.equal(Y, predicted)) * 100}%')
+    # 边界图
+    plt.subplot(2,2,1)
+    plot_decision_boundary(lambda x:lr.predict(x.T), X, Y)   # 边界是用等高线绘制的
+    # 迭代代价图
+    plt.subplot(2,2,2)
+    x = [xx for xx in range(N)]
+    plt.plot(x, lr.costs)
+
+
+
+    # 2.神经网络的逻辑回归
     nn_lr = NeuralNetworksLogisticRegression()
     nn_lr.fit(X, Y, n_h=4)
     nn_lr.train(learning_rate=0.05, num_iter=N)
     predicted = nn_lr.predict(X)
+    print(f'神经网络逻辑归回的准确率为:{np.mean(np.equal(Y, predicted)) * 100} %')
 
-    print(f'神经网络逻辑回归准确率为:{np.mean(np.equal(Y, predicted)) * 100} %')
-
-    # 决策边界图
-    plt.subplot(1,2,1)
+    # 边界图
+    plt.subplot(2,2,3)
     plot_decision_boundary(lambda x:nn_lr.predict(x.T), X, Y)   # 边界是用等高线绘制的
     # 迭代代价图
-    plt.subplot(1,2,2)
+    plt.subplot(2,2,4)
     x = [xx for xx in range(N)]
     plt.plot(x, nn_lr.costs)
     plt.show()
-    plt.show()
-
-
-
-    '''
-    # 逻辑回归的效果
-    import sklearn
-    import sklearn.datasets
-    from sklearn import linear_model
-
-    # 查看logistic回归分类效果
-    clf = linear_model.LogisticRegressionCV()
-    clf.fit(X.T, Y.T)
-
-    # 显示
-    # 显示数据集点图
-    plt.scatter(X[0, :], X[1, :], c=np.squeeze(Y), s=40, cmap=plt.cm.Spectral)
-    # 显示分类图
-    plot_decision_boundary(lambda x: clf.predict(x), X, Y)  # 绘制决策边界
-    lr_predictions = clf.predict(X.T)   # 预测结果
-    print ("逻辑回归的准确性： %d " % float((np.dot(Y, lr_predictions) + np.dot(1 - Y,1 - lr_predictions)) / float(Y.size) * 100) + "% " + "(正确标记的数据点所占的百分比)")
-
-    plt.show()
-    '''
