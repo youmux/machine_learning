5a7348bdbd9af3c0a2615c309009f65f8d12549c
youmux <1018472917@qq.com>
2019-01-09 11:05:35 +0800

    神经网络逻辑回归代码修改注释

--- codes/neural_networks_logistic_regression.py
+++ codes/neural_networks_logistic_regression.py
@@ -2,14 +2,16 @@ import numpy as np
 
 class NeuralNetworksLogisticRegression:
     '''
-    逻辑回归
-    # !不同于线性回归的地方,用'# !'标注了
+    神经网络逻辑回归
 
     参数:
         X - 训练集(需要将训练集的特征缩放到合适范围,并将参数以列向量重排)
         Y - 训练集的结果(取值为0|1)
-        W - 假设函数的多参数组成矩阵(w1、w2、w3 ...)(W_j对应theta_j,j=1,2,3...)
-        b - 假设函数的参数(x0 = 1的值)(b对应theta_0)
+        m - 样本数量
+        W1 - 输入层的权重矩阵
+        b1 - 输入层的常数项权重
+        W2 - 隐藏层的权重矩阵
+        b2 - 隐藏层的常数项权重
         learning_rate - 学习速率
         num_iter - 迭代次数
         costs - 代价函数值的集合(非必须操作)
@@ -18,10 +20,10 @@ class NeuralNetworksLogisticRegression:
         n_y - 输出层的数量
     
     使用:
-        lg = NeuralNetworksLogisticRegression()
-        lg.fit(X_train, Y_train)
-        lg.train(0.001, 2000)
-        predicted = lg.predict(X_test)
+        nn_lr = NeuralNetworksLogisticRegression()
+        nn_lr.fit(X, Y, n_h=4)
+        nn_lr.train(learning_rate=0.05, num_iter=1000)
+        predicted = nn_lr.predict(X)
     '''
     X = 0
     Y = 0
@@ -44,11 +46,15 @@ class NeuralNetworksLogisticRegression:
         1.加载训练集,并设置一些初始值
         2.设置神经网络的结构，对每层设置数量
         网络结构如下:(这里仅有1个隐藏层,且隐藏层为4个节点)
-        [输入层] [隐藏层] [输出层]
-                    o
-            o       o       o
-            o       o
-                    o
+        [输入层]         [隐藏层]         [输出层]
+        [A1]            [A2]            [A3]
+                            o
+            o               o            o
+            o               o
+                            o
+
+        [输入层的激活函数] [隐藏层的激活函数]
+        g1(x)=tanh(z)   g2(x)=1/(1+np.exp(-z))
         3.用随机数矩阵初始化权重矩阵，用零矩阵初始化偏移
 
         参数:
@@ -56,7 +62,7 @@ class NeuralNetworksLogisticRegression:
             Y - 训练集的结果
         
         注意:
-            神经网络中,不能简单将每层的权重设为0
+            神经网络中,不能简单将每层的权重设为0,否则无法每个节点权值一样无法实现功能
         '''
         self.X = X
         self.Y = Y
@@ -141,7 +147,7 @@ class NeuralNetworksLogisticRegression:
     # 梯度下降
     def gradient_descent(self):
         '''
-        进行梯度下降的运算,公式:W_j = W_j - alpha * partial_derivative(J_j(W_j, b)), j = 1,2,3...
+        进行梯度下降的运算,公式:W = W - alpha * partial_derivative(J(W, b))
         '''
         for i in range(self.num_iter):
             cache = self.forward_propagation(self.X)
@@ -179,7 +185,7 @@ class NeuralNetworksLogisticRegression:
         '''
         self.learning_rate = learning_rate
         self.num_iter = num_iter
-
+        
         self.gradient_descent()
 
 
